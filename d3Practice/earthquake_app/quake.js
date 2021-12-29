c12ded100891421f3464dd0c9d3aebeaf3aea5ae
TJ Lawless <87883465+tommyjohnny36@users.noreply.github.com>
2021-12-29 17:31:19 -0500

    updated .js

--- d3Practice/earthquake_app/quake.js
+++ d3Practice/earthquake_app/quake.js
@@ -2,7 +2,8 @@ const canvas = d3.select(".canva");
 
 var width = "100%";
 var height = "100%";
-const api_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+// const api_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson"
+const api_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 
 const svg = canvas.append("svg")
                     .attr('width', width)
@@ -28,10 +29,24 @@ d3.json(api_url)
                 .append("circle")
                 .attr("cx", (d, i) => Math.floor(Math.random() * 200) + d.properties.mag*i)
                 .attr("cy", (d, i) => Math.floor(Math.random() * 100) + d.properties.mag)
-                    .attr("r", (d, i) => (d.properties.mag) * 2) 
+                .attr("r", (d, i) => (d.properties.mag) * 2)
+                .style('top', 156)
+                .on("mouseover", function(d, i, n){
+                    d3.select(n[i])
+                    .transition()
+                    .duration(100) // in milliseconds
+                    .style("opacity", 0.7)
+                })
+                .on("mouseout", function(d, i, n)
+                    d3.select(n[i])
+                    .transition()
+                    .duration(100) // in milliseconds
+                    .style("opacity", 0.7))
                     .attr("fill", (d, i) => d.properties.alert);
 
     });
+
+    
 
 
 
